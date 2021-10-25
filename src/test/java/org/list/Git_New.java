c8a3a283c77f8762528040ccf337c3241c27dd36
ArunPrasath5004 <arunprasath5004@gmail.com>
2021-10-25 07:24:22 +0530

    updated now

--- src/test/java/org/list/Git_New.java
+++ src/test/java/org/list/Git_New.java
@@ -1,11 +1,15 @@
 package org.list;
 
+
 public class Git_New {
 	public static void main(String[] args) {
 		System.out.println("Arun Prasath");
 		System.out.println("Ajay Surya");
 		System.out.println("Subramani");
 		System.out.println("Ashwin");
+		System.out.println("giri");
+		System.out.println("harsha");
+	
 	}
 
 }
